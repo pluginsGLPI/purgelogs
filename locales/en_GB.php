ffcc7acefd77135b26fbff7b31df0b29059a8a47
walid <walid@fef7e997-3485-4979-8c08-772d0d31c224>
2012-12-10 17:00:38 +0000

    Add sources
    git-svn-id: https://forge.indepnet.net/svn/purgelogs/branches/0.80-bugfixes@1 fef7e997-3485-4979-8c08-772d0d31c224

---
+++ locales/en_GB.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ * @version $Id$
+ LICENSE
+
+  This file is part of the purgelogs plugin.
+
+ Order plugin is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ Order plugin is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GLPI; along with purgelogs. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ @package   purgelogs
+ @author    the purgelogs plugin team
+ @copyright Copyright (c) 2010-2011 purgelogs plugin team
+ @license   GPLv2+
+            http://www.gnu.org/licenses/gpl.txt
+ @link      https://forge.indepnet.net/projects/purgelogs
+ @link      http://www.glpi-project.org/
+ @since     2009
+ ---------------------------------------------------------------------- */
+
+$LANG['plugin_purgelogs']['title'][1] = "Purge history";
+$LANG['plugin_purgelogs'][1] = "Installation/désinstallation of software on computers";
+$LANG['plugin_purgelogs'][2] = "Installation/désinstallation versions on softwares";
+$LANG['plugin_purgelogs'][3] = "Add/remove profiles to users";
+$LANG['plugin_purgelogs'][4] = "dd/remove groups to users";
+$LANG['plugin_purgelogs'][5] = "Add financial information to an item";
+$LANG['plugin_purgelogs'][7] = "Logs connexions des services web";
+$LANG['plugin_purgelogs'][8] = "OCS ID Change";
+$LANG['plugin_purgelogs'][9] = "Add from OCS";
+$LANG['plugin_purgelogs'][10] = "Link with OCS";
+$LANG['plugin_purgelogs'][11] = "Delete from OCS";
+$LANG['plugin_purgelogs'][12] = "Add component";
+$LANG['plugin_purgelogs'][13] = "Update component";
+$LANG['plugin_purgelogs'][14] = "Delete component";
+$LANG['plugin_purgelogs'][15] = "Connect a component";
+$LANG['plugin_purgelogs'][16] = "Disconnect a component";
+$LANG['plugin_purgelogs'][17] = "Add relation between items";
+$LANG['plugin_purgelogs'][18] = "Delete relation between items";
+$LANG['plugin_purgelogs'][19] = "Logs purge configuration";
